aaac0367e1c03a699542efc24be9063034436281
Ajifowobajeisaac <ajifowobajeisaac@gmail.com>
2023-12-26 17:41:22 +0000

    testing console implemtation

--- console.py
+++ console.py
@@ -18,102 +18,118 @@ from models.place import Place
 class HBNBCommand(cmd.Cmd):
     """HBNBCommand class for the command interpreter"""
     prompt = '(hbnb) '
-    classes = {"BaseModel": BaseModel, "User": User, "State": State,
+    __classes = {"BaseModel": BaseModel, "User": User, "State": State,
                "City": City, "Place": Place}
+
 
     def do_create(self, args):
         """Creates a new instance of BaseModel, saves it (to the JSON file) and
          prints the id"""
-        args = args.split()
-        if len(args) == 0:
-            print("** class name missing **")
-        elif args[0] not in self.classes:
-            print("** class doesn't exist **")
-        else:
-            new_instance = self.classes[args[0]]()
-            new_instance.save()
-            print(new_instance.id)
+        try:
+            args = args.split()
+            if len(args) == 0:
+                print("** class name missing **")
+            elif args[0] not in self.__classes:
+                print("** class doesn't exist **")
+            else:
+                new_instance = self.__classes[args[0]]()
+                new_instance.save()
+                print(new_instance.id)
+        except Exception as e:
+            pass
 
     def do_show(self, args):
         """Prints the string representation of an instance based on the class
          name and id"""
-        args = args.split()
-        if len(args) == 0:
-            print("** class name missing **")
-        elif args[0] not in self.classes:
-            print("** class doesn't exist **")
-        elif len(args) == 1:
-            print("** instance id missing **")
-        else:
-            key = args[0] + "." + args[1]
-            if key not in storage.all():
-                print("** no instance found **")
+        try:
+            args = args.split()
+            if len(args) == 0:
+                print("** class name missing **")
+            elif args[0] not in self.__classes:
+                print("** class doesn't exist **")
+            elif len(args) == 1:
+                print("** instance id missing **")
             else:
-                print(storage.all()[key])
+                key = args[0] + "." + args[1]
+                if key not in storage.all():
+                    print("** no instance found **")
+                else:
+                    print(storage.all()[key])
+        except Exception as e:
+            pass
+
 
     def do_destroy(self, args):
         """Deletes an instance based on the class name and id (save the change
           into the JSON file)"""
-        args = args.split()
-        if len(args) == 0:
-            print("** class name missing **")
-        elif args[0] not in self.classes:
-            print("** class doesn't exist **")
-        elif len(args) == 1:
-            print("** instance id missing **")
-        else:
-            key = args[0] + "." + args[1]
-            if key not in storage.all():
-                print("** no instance found **")
+        try:
+            args = args.split()
+            if len(args) == 0:
+                print("** class name missing **")
+            elif args[0] not in self.__classes:
+                print("** class doesn't exist **")
+            elif len(args) == 1:
+                print("** instance id missing **")
             else:
-                del storage.all()[key]
-                storage.save()
+                key = args[0] + "." + args[1]
+                if key not in storage.all():
+                    print("** no instance found **")
+                else:
+                    del storage.all()[key]
+                    storage.save()
+        except Exception as e:
+            pass
 
     def do_all(self, args):
         """Prints all string representation of all instances based or not on
         the class name"""
 
-        args = args.split()
-        if len(args) > 0 and args[0] not in self.classes:
-            print("** class doesn't exist **")
-        else:
-            print(
-                [str(v) for k, v in storage.all().items() if not
-                    args or k.split('.')[0] == args[0]]
-                )
+        try:
+            args = args.split()
+            if len(args) > 0 and args[0] not in self.__classes:
+                print("** class doesn't exist **")
+            else:
+                print(
+                    [str(v) for k, v in storage.all().items() if not
+                        args or k.split('.')[0] == args[0]]
+                    )
+        except Exception as e:
+            pass
 
     def do_update(self, args):
         """Updates an instance based on the class name and id by
           adding or updating attribute (save the change into the JSON file)"""
-        args = args.split()
-        if len(args) == 0:
-            print("** class name missing **")
-        elif args[0] not in self.classes:
-            print("** class doesn't exist **")
-        elif len(args) == 1:
-            print("** instance id missing **")
-        else:
-            key = args[0] + "." + args[1]
-            if key not in storage.all():
-                print("** no instance found **")
-            elif len(args) == 2:
-                print("** attribute name missing **")
-            elif len(args) == 3:
-                print("** value missing **")
+        try:
+            args = args.split()
+            if len(args) == 0:
+                print("** class name missing **")
+            elif args[0] not in self.__classes:
+                print("** class doesn't exist **")
+            elif len(args) == 1:
+                print("** instance id missing **")
             else:
-                if args[2] in ['id', 'created_at', 'updated_at']:
-                    print("** attribute can't be updated **")
+                key = args[0] + "." + args[1]
+                if key not in storage.all():
+                    print("** no instance found **")
+                elif len(args) == 2:
+                    print("** attribute name missing **")
+                elif len(args) == 3:
+                    print("** value missing **")
                 else:
-                    attr_type = type(
-                        getattr(storage.all()[key], args[2], ""))
-                    if attr_type in [int, float, str]:
-                        setattr(
-                            storage.all()[key], args[2], attr_type(args[3])
-                            )
-                        storage.all()[key].save()
+                    if args[2] in ['id', 'created_at', 'updated_at']:
+                        print("** attribute can't be updated **")
                     else:
-                        print("** attribute type not allowed **")
-
+                        attr_type = type(
+                            getattr(storage.all()[key], args[2], ""))
+                        if attr_type in [int, float, str]:
+                            setattr(
+                                storage.all()[key], args[2], attr_type(args[3])
+                                )
+                            storage.all()[key].save()
+                        else:
+                            print("** attribute type not allowed **")
+        except Exception as e:
+           pass
 
     def do_quit(self, args):
         """Quit command to exit the program"""
