d4879f9e8359faba20664ef0962a513a9a64bbf9
Ajifowobajeisaac <ajifowobajeisaac@gmail.com>
2023-12-26 14:10:54 +0000

    modified the init ialization l

--- models/base_model.py
+++ models/base_model.py
@@ -29,30 +29,21 @@ class BaseModel:
           instance.
     """
     def __init__(self, *args, **kwargs):
+       
         """Initializes a new BaseModel instance
         Args:
             *args: unused
             **kwargs: Arbitrary keyword arguments.
         """
-
         if kwargs:
             for key, value in kwargs.items():
-                if key == 'created_at' or key == 'updated_at':
-                    if not isinstance(value, datetime):
-                        value = datetime.fromisoformat(value)
-                if key != '__class__':
+                if key != "__class__":
+                    if key == "created_at" or key == "updated_at":
+                        value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                     setattr(self, key, value)
-            if 'id' not in kwargs:
-                self.id = str(uuid.uuid4())
-            if 'created_at' not in kwargs:
-                self.created_at = datetime.now()
-            if 'updated_at' not in kwargs:
-                self.updated_at = datetime.now()
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            models.storage.new(self)
 
     def __str__(self):
         """str method -
