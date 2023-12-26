e7b72bad25838bf6f995f42e7406ac559e10cfc4
Ajifowobajeisaac <ajifowobajeisaac@gmail.com>
2023-12-26 14:52:35 +0000

    fixed reload bug

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,6 +5,10 @@ and deserialization of all your data, to and from a JSON file.
 """
 import json
 from models import base_model
+from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
 
 
 class FileStorage:
@@ -29,16 +33,18 @@ class FileStorage:
 
     def reload(self):
         """Deserialize the JSON file __file_path to __objects, if it exists."""
+        class_dict = {"BaseModel": base_model.BaseModel, "User": User,
+                      "State": State, "City": City, "Place": Place}
+        
         try:
             with open(FileStorage.__file_path) as f:
                 objdict = json.load(f)
                 for o in objdict.values():
                     cls_name = o["__class__"]
                     del o["__class__"]
-                    if cls_name in globals():
-                        global_class = globals()[cls_name]
-                        if issubclass(global_class, base_model.BaseModel):
-                            self.new(global_class(**o))
+                    if cls_name in class_dict:
+                        if issubclass(class_dict[cls_name], base_model.BaseModel):
+                            self.new(class_dict[cls_name](**o))
         except FileNotFoundError:
             print("File not found")
         except json.decoder.JSONDecodeError:
