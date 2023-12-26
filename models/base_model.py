3dbe2588d2aa95e4738df06eee645298a1cce3a7
Ajifowobajeisaac <ajifowobajeisaac@gmail.com>
2023-12-26 14:12:40 +0000

    fixed base_model codesytle

--- models/base_model.py
+++ models/base_model.py
@@ -29,7 +29,6 @@ class BaseModel:
           instance.
     """
     def __init__(self, *args, **kwargs):
-       
         """Initializes a new BaseModel instance
         Args:
             *args: unused
@@ -39,11 +38,13 @@ class BaseModel:
             for key, value in kwargs.items():
                 if key != "__class__":
                     if key == "created_at" or key == "updated_at":
-                        value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                        value = datetime.strptime(
+                            value, "%Y-%m-%dT%H:%M:%S.%f")
                     setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         """str method -
