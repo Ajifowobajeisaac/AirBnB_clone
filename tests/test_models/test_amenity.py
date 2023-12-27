2c9db2653c247d5abd7bdbf98147bec122ae1ac8
Ajifowobajeisaac <ajifowobajeisaac@gmail.com>
2023-12-27 08:41:15 +0000

    added tests for amenity

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,64 +1,106 @@
-#!/usr/bin/python3
+#!/ust/bin/python3
 
 import unittest
-from models.review import Review
-from models.base_model import BaseModel
+from models.amenity import Amenity
 from datetime import datetime
 from time import sleep
 
-class TestReview(unittest.TestCase):
-    """Unittests for testing instantiation of the Review class."""
+class TestAmenity(unittest.TestCase):
+    """Unittests for testing instantiation of the Amenity class."""
 
     def setUp(self):
         """
         Set up for the tests.
         """
-        self.review = Review()
+        self.amenity = Amenity()
 
     def tearDown(self):
         """
         Clean up after the tests.
         """
-        del self.review
+        del self.amenity
 
     def test_no_args_instantiates(self):
-        self.assertEqual(Review, type(Review()))
+        self.assertEqual(Amenity, type(Amenity()))
 
     def test_id_is_public_str(self):
-        self.assertEqual(str, type(Review().id))
+        self.assertEqual(str, type(Amenity().id))
 
     def test_created_at_is_public_datetime(self):
-        self.assertEqual(datetime, type(Review().created_at))
+        self.assertEqual(datetime, type(Amenity().created_at))
 
     def test_updated_at_is_public_datetime(self):
-        self.assertEqual(datetime, type(Review().updated_at))
+        self.assertEqual(datetime, type(Amenity().updated_at))
 
-    def test_two_reviews_unique_ids(self):
-        r1 = Review()
-        r2 = Review()
-        self.assertNotEqual(r1.id, r2.id)
+    def test_two_amenities_unique_ids(self):
+        a1 = Amenity()
+        a2 = Amenity()
+        self.assertNotEqual(a1.id, a2.id)
 
-    def test_two_reviews_different_created_at(self):
-        r1 = Review()
+    def test_two_amenities_different_created_at(self):
+        a1 = Amenity()
         sleep(0.05)
-        r2 = Review()
-        self.assertLess(r1.created_at, r2.created_at)
+        a2 = Amenity()
+        self.assertLess(a1.created_at, a2.created_at)
 
-    def test_two_reviews_different_updated_at(self):
-        r1 = Review()
+    def test_two_amenities_different_updated_at(self):
+        a1 = Amenity()
         sleep(0.05)
-        r2 = Review()
-        self.assertLess(r1.updated_at, r2.updated_at)
+        a2 = Amenity()
+        self.assertLess(a1.updated_at, a2.updated_at)
 
     def test_str_representation(self):
         dt = datetime.today()
         dt_repr = repr(dt)
-        r = Review()
-        r.id = "123456"
-        r.created_at = r.updated_at = dt
-        rstr = r.__str__()
-        self.assertIn("[Review] (123456)", rstr)
+        a = Amenity()
+        a.id = "123456"
+        a.created_at = a.updated_at = dt
+        astr = a.__str__()
+        self.assertIn("[Amenity] (123456)", astr)
+        self.assertIn("'id': '123456'", astr)
+        self.assertIn("'created_at': " + dt_repr, astr)
+        self.assertIn("'updated_at': " + dt_repr, astr)
 
+    def test_instantiation_with_kwargs(self):
+        dt = datetime.today()
+        dt_iso = dt.isoformat()
+        a = Amenity(id="345", created_at=dt_iso, updated_at=dt_iso)
+        self.assertEqual(a.id, "345")
+        self.assertEqual(a.created_at, dt)
+        self.assertEqual(a.updated_at, dt)
 
-if __name__ == "__main__":
+    def test_instantiation_with_None_kwargs(self):
+        with self.assertRaises(TypeError):
+            Amenity(id=None, created_at=None, updated_at=None)
+
+    def test_instantiation_with_args_and_kwargs(self):
+        dt = datetime.today()
+        dt_iso = dt.isoformat()
+        a = Amenity("12", id="345", created_at=dt_iso, updated_at=dt_iso)
+        self.assertEqual(a.id, "345")
+        self.assertEqual(a.created_at, dt)
+        self.assertEqual(a.updated_at, dt)
+
+    def test_save_method(self):
+        a = Amenity()
+        old_updated_at = a.updated_at
+        a.save()
+        self.assertNotEqual(old_updated_at, a.updated_at)
+        with open("file.json", "r") as f:
+            self.assertIn("Amenity." + a.id, f.read())
+
+    def test_to_dict(self):
+        a = Amenity()
+        a_dict = a.to_dict()
+        self.assertEqual(dict, type(a_dict))
+        self.assertIn('__class__', a_dict)
+        self.assertEqual('Amenity', a_dict['__class__'])
+        self.assertIn('id', a_dict)
+        self.assertEqual(str, type(a_dict['id']))
+        self.assertIn('created_at', a_dict)
+        self.assertEqual(str, type(a_dict['created_at']))
+        self.assertIn('updated_at', a_dict)
+        self.assertEqual(str, type(a_dict['updated_at']))
+
+if __name__ == '__main__':
     unittest.main()
